2ac5cd44bfd01e5555116ae28292bca861e1505c
umangborad <Umang.Borad@innroad.com>
2023-09-16 17:59:28 -0400

    adding dependency injection, renaming app

--- SpeedyAir.ly/Program.cs
+++ SpeedyAir.ly/Program.cs
@@ -1,5 +1,6 @@
-﻿using SpeedyAir.ly.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SpeedyAir.ly.Application.Interface;
+using SpeedyAir.ly.ConsoleApp.Extension;
 
 namespace SpeedyAir.ly;
 
@@ -7,18 +8,31 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        IFlightScheduler flightScheduler = new FlightScheduler();
+        try
+        {
+            // Set up the dependency injection container
+            var serviceProvider = new ServiceCollection()
+                .RegisterService()
+                .BuildServiceProvider();
 
-        // User Story 1:
-        flightScheduler.LoadFlights();
-        flightScheduler.DisplayFlightSchedules();
+            var flightScheduler = serviceProvider.GetService<IFlightScheduler>()!;
 
-        // User Story 2:
-        flightScheduler.LoadOrders();
-        flightScheduler.ScheduleOrdersToFlights();
-        flightScheduler.DisplayOrders();
+            // User Story 1:
+            flightScheduler.LoadFlights();
+            flightScheduler.DisplayFlightSchedules();
 
-        // Display full flight details including current load
-        //flightScheduler.DisplayFullFlightDetails();
+            // User Story 2:
+            flightScheduler.LoadOrders();
+            flightScheduler.ScheduleOrdersToFlights();
+            flightScheduler.DisplayOrders();
+
+            // Display full flight details including current load
+            //flightScheduler.DisplayFullFlightDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
